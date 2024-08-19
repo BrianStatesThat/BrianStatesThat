22d28e847eae1667b2f53a588330157ba21fc1d0
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2024-08-19 17:04:02 +0200

    a

--- scipt.js
+++ scipt.js
@@ -1,9 +1,7 @@
 /* Menu */
-document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelectorAll(".menu-toggle");
     const menu = document.querySelectorAll(".menu");
 
     menuToggle.addEventListener("click", () => {
         menu.classList.toggle('open');
     });
-});
