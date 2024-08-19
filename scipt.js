799e283c172b155d4909c41f924e6e6e0f688135
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2024-08-19 23:48:45 +0200

    a

--- scipt.js
+++ scipt.js
@@ -1,8 +1,10 @@
 /*Menu List*/
 function openMenu() {
-    document.getElementById("menu").style.top = "0";
+  document.getElementById("menu").style.top = "0";
+  document.getElementById("menu").style.display = "flex"
   }
   
   function closeMenu() {
     document.getElementById("menu").style.top = "-100vh";
+    document.getElementById("menu").style.display = "fixed";
   }
