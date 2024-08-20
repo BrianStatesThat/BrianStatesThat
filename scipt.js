1815e82216bee8b8f8fc081e6cad4e5e4faa046d
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2024-08-20 09:30:35 +0200

    a

--- scipt.js
+++ scipt.js
@@ -3,9 +3,9 @@
 const menuList = document.getElementById("menu");
 
 const openBtn = () => {
-    menuList.style.left("0");
+    menuList.style.left="0";
 }
 
 const closeBtn = () => {
-    menuList.style.left("-250px");
+    menuList.style.left="-250px";
 }
